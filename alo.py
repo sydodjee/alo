38ef24b33e328d5cba09710120f86fc1037fa451
sydodjee <125597093+sydodjee@users.noreply.github.com>
2024-12-01 22:24:04 +0400

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,8 +1,10 @@
 from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
 BOT_TOKEN = '7649317053:AAEuahOjsqpu2aqQGs5qlJCsKvL35qU-leo'
+WEBHOOK_URL = f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}"  # Render автоматически предоставит URL
 
 async def call_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
@@ -14,4 +16,11 @@ async def call_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler('alo', call_command))
-    app.run_polling()
+
+    # Настраиваем webhook
+    app.run_webhook(
+        listen="0.0.0.0", 
+        port=int(os.environ.get("PORT", 8443)), 
+        url_path=BOT_TOKEN,
+        webhook_url=f"{WEBHOOK_URL}/{BOT_TOKEN}"
+    )
